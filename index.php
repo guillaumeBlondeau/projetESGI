da1725f87a76411c5a1db0b1725bdad3286825d0
Guillaume.B <guillome6@hotmail.fr>
2015-04-08 14:46:51 +0200

    Changement nom

--- index.php
+++ index.php
@@ -22,52 +22,49 @@
 
 <!doctype html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <title>Hello</title>
-    <script>
-	  window.fbAsyncInit = function() {
-		FB.init({
-		  appId      : '<?php echo APPID; ?>',
-		  xfbml      : true,
-		  version    : 'v2.3'
-		});
-	  };
-	
-	  (function(d, s, id){
-		 var js, fjs = d.getElementsByTagName(s)[0];
-		 if (d.getElementById(id)) {return;}
-		 js = d.createElement(s); js.id = id;
-		 js.src = "//connect.facebook.net/fr_FR/sdk.js";
-		 fjs.parentNode.insertBefore(js, fjs);
-	   }(document, 'script', 'facebook-jssdk'));
-	</script>
-</head>
+	<head>
+	    <meta charset="utf-8">
+	    <title>Hello</title>
+	    <script>
+		  window.fbAsyncInit = function() {
+			FB.init({
+			  appId      : '<?php echo APPID; ?>',
+			  xfbml      : true,
+			  version    : 'v2.3'
+			});
+		  };
+		
+		  (function(d, s, id){
+			 var js, fjs = d.getElementsByTagName(s)[0];
+			 if (d.getElementById(id)) {return;}
+			 js = d.createElement(s); js.id = id;
+			 js.src = "//connect.facebook.net/fr_FR/sdk.js";
+			 fjs.parentNode.insertBefore(js, fjs);
+		   }(document, 'script', 'facebook-jssdk'));
+		</script>
+	</head>
+	<body>
+		<div
+	      class="fb-like"
+	      data-share="true"
+	      data-width="850"
+	      data-show-faces="true">
+	    </div>
+	    <hr>
+	    <?php
+			$loginUrl = $helper->getLoginUrl();
 
-<body>
-	<div
-      class="fb-like"
-      data-share="true"
-      data-width="850"
-      data-show-faces="true">
-    </div>
-    <hr>
-    <?php
-		$loginUrl = $helper->getLoginUrl();
-
-		if ($session) {
-			try {
-				$user_profile = (new FacebookRequest(
-					$session, 'GET', '/me'
-				))->execute()->getGraphObject(GraphUser::className());
-				echo "Name : ".$user_profile->getName();
-			} catch(FacebookRequestException $e) {
-				echo "Exception occured, code : ".$e->getCode();
-				echo " with message : ".$e->getMessage();
+			if ($session) {
+				try {
+					$user_profile = (new FacebookRequest($session, 'GET', '/me'))->execute()->getGraphObject(GraphUser::className());
+					echo "Bonjour ".$user_profile->getName()." !";
+				} catch(FacebookRequestException $e) {
+					echo "Exception occured, code : ".$e->getCode();
+					echo " with message : ".$e->getMessage();
+				}
+			} else {
+				echo "Not connected. Please <a href=".$loginUrl.">Se connecter</a>";
 			}
-		} else {
-			echo "Not connected. Please <a href=".$loginUrl.">Se connecter</a>";
-		}
-	?>
-</body>
+		?>
+	</body>
 </html>
